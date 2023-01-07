614931c9501155d6f7ece22a08e2241756f984c0
“wws” <1274704958@qq.com>
2023-01-07 15:05:41 +0800

    test server done

--- src/test_ser/main.cpp
+++ src/test_ser/main.cpp
@@ -5,7 +5,7 @@
 #include <lsquic.h>
 #include <asio.hpp>
 
-
+static bool IsRunning = true;
 class Context
 {
 public:
@@ -17,7 +17,16 @@ public:
     }
     asio::io_context io_context;
     std::shared_ptr<asio::ip::udp::socket> socket;
+    std::shared_ptr< asio::steady_timer> timer;
 };
+
+void sig_handler(int sig)
+{
+    if (sig == SIGINT)
+    {
+        IsRunning = false;
+    }
+}
 
 static SSL_CTX* s_ssl_ctx;
 
@@ -34,9 +43,13 @@ lsquic_conn_ctx_t* on_new_conn(void* stream_if_ctx,lsquic_conn_t* c);
 void on_conn_closed(lsquic_conn_t* c);
 lsquic_stream_ctx_t* on_new_stream(void* stream_if_ctx, lsquic_stream_t* s);
 void on_read(lsquic_stream_t* s, lsquic_stream_ctx_t* h);
+void on_write(lsquic_stream_t* s, lsquic_stream_ctx_t* h);
+void on_close(lsquic_stream_t* s, lsquic_stream_ctx_t* h);
+void process_conns(lsquic_engine_t* engine, Context& cxt);
 //main /////////////
 int main(int argc,const char** argv)
 {
+    signal(SIGINT, sig_handler);
     if (0 != lsquic_global_init(LSQUIC_GLOBAL_SERVER))
     {
         printf("init lsquic failed!!!\n");
@@ -49,6 +62,8 @@ int main(int argc,const char** argv)
     stream_if.on_conn_closed = on_conn_closed;
     stream_if.on_new_stream = on_new_stream;
     stream_if.on_read = on_read;
+    stream_if.on_write = on_write;
+    stream_if.on_close = on_close;
 
     lsquic_engine_api engine_api = {};
     engine_api.ea_packets_out = packets_out;
@@ -56,8 +71,33 @@ int main(int argc,const char** argv)
     engine_api.ea_stream_if = &stream_if;
     engine_api.ea_stream_if_ctx = &context;
     engine_api.ea_get_ssl_ctx = get_ssl_ctx;
-    //lsquic_engine_t* engine = lsquic_engine_new(LSENG_SERVER, &engine_api);
-    context.io_context.run();
+    lsquic_engine_t* engine = lsquic_engine_new(LSENG_SERVER, &engine_api);
+    while (IsRunning)
+    {
+        std::array<char, 1500> buffer;
+        // 异步接收数据
+        asio::ip::udp::endpoint sender_endpoint;
+        context.socket->async_receive_from(
+            asio::buffer(buffer), sender_endpoint,
+            [&](const asio::error_code& error, std::size_t bytes_transferred)
+            {
+                if (!error)
+                {
+                    std::cout << "Received " << bytes_transferred << " bytes.\n";
+                    auto local = context.socket->local_endpoint();
+                    int ret = ::lsquic_engine_packet_in(engine, reinterpret_cast<const unsigned char*>(buffer.data()), bytes_transferred,
+                        local.data(), sender_endpoint.data(), nullptr, 0);
+                    printf("lsquic_engine_packet_in %d", ret);
+                    process_conns(engine,context);
+                }
+                else
+                    std::cout << "Receive get error " << error.message() << std::endl;
+            }
+        );
+        context.io_context.run_for(std::chrono::seconds(1));
+    }
+    
+    printf("end\n");
     lsquic_global_cleanup();
 
     if (s_ssl_ctx)
@@ -66,6 +106,24 @@ int main(int argc,const char** argv)
         s_ssl_ctx = nullptr;
     }
     return 0;
+}
+void process_conns(lsquic_engine_t* engine, Context& cxt)
+{
+    if (cxt.timer)cxt.timer->cancel();
+    int diff;
+    int timeout;
+    lsquic_engine_process_conns(engine);
+    if (lsquic_engine_earliest_adv_tick(engine, &diff)) {
+        if (diff > 0)
+            timeout = diff / 1000;   /* To seconds */
+        else
+            timeout = 0;
+        cxt.timer = std::make_shared< asio::steady_timer>(cxt.io_context, std::chrono::milliseconds(timeout));
+        cxt.timer->async_wait([&](const asio::error_code& error) {
+            if (!error)
+                process_conns(engine, cxt);
+        });
+    }
 }
 
 int packets_out(
@@ -108,18 +166,29 @@ void on_conn_closed(lsquic_conn_t* c)
 lsquic_stream_ctx_t* on_new_stream(void* stream_if_ctx, lsquic_stream_t* s)
 {
     ::lsquic_stream_wantread(s, 1);
+    printf("stream create %zx\n", (size_t)s);
     return nullptr;
 }
 void on_read(lsquic_stream_t* s, lsquic_stream_ctx_t* h)
 {
-	char buf[1024] = {0};
+	char buf[1500] = {0};
     std::string str;
     int len = 0;
 	while ((len = ::lsquic_stream_read(s, buf, sizeof(buf))) > 0)
 	{
 		str.append(buf,len);
 	}
-    printf("recv str = %s\n",str.c_str());
+    ::lsquic_stream_wantwrite(s, 1);
+    printf("recv %zx str = %s\n",(size_t)s, str.c_str());
+}
+void on_write(lsquic_stream_t* s, lsquic_stream_ctx_t* h)
+{
+    const char* str = "Hi! client";
+    ::lsquic_stream_write(s, str, strlen(str));
+}
+void on_close(lsquic_stream_t* s, lsquic_stream_ctx_t* h)
+{
+    printf("stream closed %zx\n",(size_t)s);
 }
 static int load_cert(const char* cert_file, const char* key_file)
 {
