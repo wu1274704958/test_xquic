f5413081d96f261069df7964f144ba9826d87758
wws <1274704958@qq.com>
2023-01-14 12:55:02 +0800

    build add args build type

--- build.sh
+++ build.sh
@@ -1,1 +1,6 @@
-cmake -G Ninja -S . -B build -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=/home/lighthouse/code/vcpkg/scripts/buildsystems/vcpkg.cmake -DSSL_PATH=/home/lighthouse/code/vcpkg/installed/x64-linux -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++
+BuildType="Debug"
+if [ $# -ge 1 ];then 
+	BuildType=$1
+fi
+cmake -G Ninja -S . -B build -DCMAKE_BUILD_TYPE=$BuildType -DCMAKE_TOOLCHAIN_FILE=/home/lighthouse/code/vcpkg/scripts/buildsystems/vcpkg.cmake -DSSL_PATH=/home/lighthouse/code/vcpkg/installed/x64-linux -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DBUILD_MQAS_EXAMPLES=ON -DBUILD_SHARED_LIBS=ON
+cmake --build ./build
