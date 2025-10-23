85327688ca070b0d006ae92a3efe863787adf55e
wws <1274704958@qq.com>
2025-10-23 15:27:11 +0800

    Fix find boringssl script

--- cmake/Findboringssl.cmake
+++ cmake/Findboringssl.cmake
@@ -1,16 +1,18 @@
 message("Find boringssl")
 if(NOT SSL_PATH)
     message("No SSL_PATH find")
-    find_path(boringssl_INCLUDE_DIR openssl REQUIED)
-    find_library(boringssl_ssl_LIBRARY ssl REQUIED)
-    find_library(boringssl_crypto_LIBRARY crypto REQUIED)
+    find_path(boringssl_INCLUDE_DIR openssl REQUIRED)
+    find_library(boringssl_ssl_LIBRARY ssl REQUIRED)
+    find_library(boringssl_crypto_LIBRARY crypto REQUIRED)
     set(boringssl_LIBRARY
         ${boringssl_ssl_LIBRARY}
         ${boringssl_crypto_LIBRARY}
     )
+    
+    set(VCPKG_BINARAY_DIR "${VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/bin")
 
-    find_file(boringssl_ssl_BINARY "ssl${CMAKE_SHARED_LIBRARY_SUFFIX}" REQUIED)
-    find_file(boringssl_crypto_BINARY "crypto${CMAKE_SHARED_LIBRARY_SUFFIX}" REQUIED)
+    find_file(boringssl_ssl_BINARY "ssl${CMAKE_SHARED_LIBRARY_SUFFIX}" PATHS ${VCPKG_BINARAY_DIR} REQUIRED)
+    find_file(boringssl_crypto_BINARY "crypto${CMAKE_SHARED_LIBRARY_SUFFIX}" PATHS ${VCPKG_BINARAY_DIR} REQUIRED)
     set(boringssl_BINARY
         ${boringssl_ssl_BINARY}
         ${boringssl_crypto_BINARY}
@@ -18,16 +20,16 @@ if(NOT SSL_PATH)
 
 else()
     message("${SSL_PATH}/include")
-    find_path(boringssl_INCLUDE_DIR openssl "${SSL_PATH}/include" REQUIED)
-    find_library(boringssl_ssl_LIBRARY ssl "${SSL_PATH}/lib" REQUIED)
-    find_library(boringssl_crypto_LIBRARY crypto "${SSL_PATH}/lib" REQUIED)
+    find_path(boringssl_INCLUDE_DIR openssl "${SSL_PATH}/include" REQUIRED)
+    find_library(boringssl_ssl_LIBRARY ssl "${SSL_PATH}/lib" REQUIRED)
+    find_library(boringssl_crypto_LIBRARY crypto "${SSL_PATH}/lib" REQUIRED)
     set(boringssl_LIBRARY
         ${boringssl_ssl_LIBRARY}
         ${boringssl_crypto_LIBRARY}
     )
 
-    find_file(boringssl_ssl_BINARY "ssl${CMAKE_SHARED_LIBRARY_SUFFIX}" "${SSL_PATH}/bin" REQUIED)
-    find_file(boringssl_crypto_BINARY "crypto${CMAKE_SHARED_LIBRARY_SUFFIX}" "${SSL_PATH}/bin" REQUIED)
+    find_file(boringssl_ssl_BINARY "ssl${CMAKE_SHARED_LIBRARY_SUFFIX}" PATHS "${SSL_PATH}/bin" REQUIRED)
+    find_file(boringssl_crypto_BINARY "crypto${CMAKE_SHARED_LIBRARY_SUFFIX}" PATHS "${SSL_PATH}/bin" REQUIRED)
     set(boringssl_BINARY
         ${boringssl_ssl_BINARY}
         ${boringssl_crypto_BINARY}
